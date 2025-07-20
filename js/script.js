a7b1ad313d6a54cac73c7a993555288d987695e9
Farah Mahmoud <faraheltahawy27@gmail.com>
2025-07-20 10:29:11 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    loop: true,
+    speed: 6500,
+    slidesPerView: "auto",
+    freeMode: true,
+    autoplay: {
+        delay: 0,
+    },
+});
